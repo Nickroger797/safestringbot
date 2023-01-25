8cb40528c61ffec848e401e003498cd578017155
Nickroger797 <115403112+Nickroger797@users.noreply.github.com>
2023-01-25 17:41:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID", "25847347"))
-API_HASH = getenv("API_HASH", "11569826ebbc9f8b14c3ab1c5ddb937b")
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
 
-BOT_TOKEN = getenv("BOT_TOKEN", "5907777474:AAELGNY2Dvau5Fn1RuKUjfQw7g9y0motfOg")
-OWNER_ID = int(getenv("OWNER_ID", "1327762724"))
+BOT_TOKEN = getenv("BOT_TOKEN")
+OWNER_ID = int(getenv("OWNER_ID"))
 
-MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://Aditgbot:0987@cluster0.wutyhoa.mongodb.net/?retryWrites=true&w=majority")
-MUST_JOIN = getenv("MUST_JOIN", "-1001682109318")
+MONGO_DB_URI = getenv("MONGO_DB_URI")
+MUST_JOIN = getenv("MUST_JOIN")
